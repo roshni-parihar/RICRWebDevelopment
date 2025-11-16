a0e9c0bd70586947ae3a5d0b35bceff264e5d1ab
roshni-spec <roshanipari577@gmail.com>
2025-11-16 18:11:52 +0530

    LoginReg form on bootstrap

---
+++ JavaScript/Basic Js/index.js
@@ -0,0 +1,20 @@
+var a = 10;
+var a = 20;
+
+let b = 15;
+
+console.log(a, b);
+
+let ar= ["abc", 1234, 45.4, 'a', true];
+console.log(ar);
+//alert("this is alert");
+console.error("this is error");
+console.warn("this is warning");
+
+
+
+if (confirm("Are you sure")) {
+console.log("thanks");
+}else{
+    console.error("it's okay");
+}
