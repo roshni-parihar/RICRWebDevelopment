069f6481df3636a90fdfb3440502a402262abcc3
roshni-spec <roshanipari577@gmail.com>
2025-11-19 18:20:16 +0530

    RICR form

---
+++ JavaScript/RICR/index.js
@@ -0,0 +1,22 @@
+function Student_Login() {
+  console.log("Student Login Button clicked");
+}
+
+function Submit() {
+  console.log("Submit Button clicked");
+  const nm = document.getElementById("PersonName").value;
+  const contact = document.getElementById("ContactNumber").value;
+  const em = document.getElementById("EmailID").value;
+  const qual = document.getElementById("Qualifications").value;
+  const clg = document.getElementById("College/School").value;
+
+
+  const br = document.getElementById("BranchName").value;
+  console.log("Name:" + nm);
+  console.log("ContactN.:" + contact);
+  console.log("Email:" + em);
+  console.log("Qualification:" + qual);
+  console.log("College/School:" + clg);
+
+  console.log("Branch:" + br);
+}
