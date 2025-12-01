ef983353f3ad42995d0d39eb4a7e299c630e77de
roshni-spec <roshanipari577@gmail.com>
2025-12-01 22:07:26 +0530

    calculator2 using try catch

--- JavaScript/Calcula/script.js
+++ JavaScript/Calcula/script.js
@@ -8,7 +8,7 @@ function Input(char) {
         document.getElementById("display").value ="";
     }
   } else if (char === "C") {
-    document.getElementById("display").value = "";
+    document.getElementById("display").value ="";
   } else {
     let exp = document.getElementById("display").value;
     exp = exp + char;
