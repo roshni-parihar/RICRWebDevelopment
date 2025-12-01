e9aeb6c2a29678a722b3906f9bef76132c626be5
roshni-spec <roshanipari577@gmail.com>
2025-12-01 22:02:38 +0530

    onclick function with light example

--- JavaScript/Events/script.js
+++ JavaScript/Events/script.js
@@ -1,17 +1,17 @@
 function On() {
-  document.getElementById("bulb").style.backgroundColor = "yellow";
+  document.getElementById("bulb").style.backgroundColor ="yellow";
 }
 function Off() {
-  document.getElementById("bulb").style.backgroundColor = "white";
+  document.getElementById("bulb").style.backgroundColor ="white";
 }
 function Red() {
-  document.getElementById("bulb").style.backgroundColor = "red";
+  document.getElementById("bulb").style.backgroundColor ="red";
 }
 function Green() {
-  document.getElementById("bulb").style.backgroundColor = "green";
+  document.getElementById("bulb").style.backgroundColor ="green";
 }
 function Blue() {
-  document.getElementById("bulb").style.backgroundColor = "blue";
+  document.getElementById("bulb").style.backgroundColor ="blue";
 }
 const userColor = document.getElementById("color");
 userColor.addEventListener("change", () => changeBulbColor(userColor.value));
