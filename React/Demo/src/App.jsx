6a64919c25e9fcb04f7f62ee0d27f41d98f8e165
roshni-spec <roshanipari577@gmail.com>
2025-12-25 14:49:38 +0530

    react basic

---
+++ React/Demo/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  let a = 5;
+  let b = 10;
+  console.log(a + b);
+
+  return (
+    <>
+      <h1 id="head">Hello World!</h1>
+      <div>this is my react app</div>
+    </>
+  );
+}
+
+export default App;
